60bb605a3a48ee307e01c05d9909260f4e68c4b9
abigailkeller <abigail.g.keller1@gmail.com>
2024-10-17 17:22:31 +0000

    added tidyverse to install.r

--- install.r
+++ install.r
@@ -1,3 +1,4 @@
 
-install.packages(c("sf", "terra", "stars", "rstan", "TMB", "nimble", "MCMCvis"))
+install.packages(c("sf", "terra", "stars", "rstan", "TMB", "nimble", "MCMCvis",
+                   "tidyverse", "bayestestR", "patchwork", "cowplot"))
 
